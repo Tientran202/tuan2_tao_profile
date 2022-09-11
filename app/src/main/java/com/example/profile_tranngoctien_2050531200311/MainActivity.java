eb7241bd806e12937f6c0521bdfd53d4307b0865
Tientran202 <tranvuongngoctien@gmail.com>
2022-09-11 16:43:35 +0700

    Bài tuần 2: Bài tập tạo giao diện profile người dùng

---
+++ app/src/main/java/com/example/profile_tranngoctien_2050531200311/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.profile_tranngoctien_2050531200311;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile3);
+    }
+}
